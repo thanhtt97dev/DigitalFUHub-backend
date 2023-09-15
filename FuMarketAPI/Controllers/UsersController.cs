5517b4e6f53d5d83617816badc00a55753d22a46
hiuhihi78 <leduchieu2001x@gmail.com>
2023-09-15 17:53:58 +0700

    fix login

--- FuMarketAPI/Controllers/UsersController.cs
+++ FuMarketAPI/Controllers/UsersController.cs
@@ -101,17 +101,16 @@
 					};
 					_userRepository.AddUser(newUser);
 					user = _userRepository.GetUserByEmail(userSignIn.Email);
-					var tokenForNewUser = await _jwtTokenService.GenerateTokenAsync(newUser);
-					return Ok(tokenForNewUser);
+					var tokenForNewUser = _jwtTokenService.GenerateTokenAsync(newUser);
+					return Ok(await tokenForNewUser);
 				}
 
 				if (!user.Status) return Conflict("Your account was baned!");
 				if (user.TwoFactorAuthentication)
 					return StatusCode(StatusCodes.Status416RangeNotSatisfiable, user.UserId);
 
-				var token = await _jwtTokenService.GenerateTokenAsync(user);
-
-				return Ok(token);
+				var token =  _jwtTokenService.GenerateTokenAsync(user);
+				return Ok(await token);
 			}
 			catch (Exception ex)
 			{
