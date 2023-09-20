5db0facb0b32e3a1f058b9170d20832b23bdd2f0
hieunv <nguyenhieuskynett@gmail.com>
2023-09-20 07:26:21 +0700

    rebuild solution

--- DTOs/SendChatMessageRequestDTO.cs
+++ DTOs/SendChatMessageRequestDTO.cs
@@ -13,5 +13,6 @@ namespace DTOs
         public long RecipientId { get; set; }
         public string Content { get; set; } = string.Empty;
         public DateTime DateCreate { get; set; }
+        public string MessageType { get; set; } = "1";
     }
 }
