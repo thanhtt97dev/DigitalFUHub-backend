29d06ceaf9ff38f804c72b611bc492e2c1f32897
tienpm <pmt11112001@gmail.com>
2023-09-27 16:11:39 +0700

    update migration db

--- BusinessObject/Entities/Feedback.cs
+++ BusinessObject/Entities/Feedback.cs
@@ -22,6 +22,6 @@ namespace BusinessObject.Entities
         public virtual Product Product { get; set; } = null!;
         [ForeignKey(nameof(UserId))]
         public virtual User User { get; set; } = null!;
-        public virtual ICollection<Media> Medias { get; set; } = null!;
+        public virtual ICollection<FeedbackMedia> FeedbackMedias { get; set; } = null!;
     }
 }
