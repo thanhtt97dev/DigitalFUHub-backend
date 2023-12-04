cb39c294e5f9f74a67e1ffb1fd26b1a199605dd5
hiuhihi78 <leduchieu2001x@gmail.com>
2023-12-04 14:40:12 +0700

    fix job schedule

---
+++ DigitalFUHubApi/Jobs/HistoryTransactionMbBankJob.cs
@@ -0,0 +1,59 @@
+﻿using Comons;
+using DataAccess.DAOs;
+using DigitalFUHubApi.Comons;
+using DigitalFUHubApi.Services;
+using DTOs.MbBank;
+using Quartz;
+using System.Text.Json;
+
+namespace DigitalFUHubApi.Jobs
+{
+	public class HistoryTransactionMbBankJob : IJob
+	{
+		private readonly IConfiguration configuration;
+		private readonly MbBankService mbBankService;
+
+		public HistoryTransactionMbBankJob(IConfiguration configuration, MbBankService mbBankService)
+		{
+			this.configuration = configuration;
+			this.mbBankService = mbBankService;
+		}
+
+		public async Task Execute(IJobExecutionContext context)
+		{
+			//return;
+			var data = await mbBankService.GetHistoryTransaction();
+
+			if (data == null) return;
+			if (data.result.responseCode != Constants.MB_BANK_RESPONE_CODE_SUCCESS) return;
+
+			List<TransactionHistory> transactionHistoryList = new List<TransactionHistory>();
+			if (data.transactionHistoryList != null)
+			{
+				transactionHistoryList = data.transactionHistoryList
+					.Where(x => x.creditAmount != 0 && x.description.Contains(Constants.BANK_TRANSACTION_CODE_KEY)).ToList();
+			}
+
+			string? directoryPathStoreData = configuration["MbBank:DirectoryPathStoreData"];
+			if (directoryPathStoreData == null) return;
+
+			string dataPreviousText = Util.ReadFile(directoryPathStoreData);
+			List<TransactionHistory>? dataPrevious = new List<TransactionHistory>();
+			if (!string.IsNullOrEmpty(dataPreviousText))
+			{
+				dataPrevious = JsonSerializer.Deserialize<List<TransactionHistory>>(dataPreviousText);
+			}
+			// Compare previous data with current data
+			bool isSame = true;
+			if (dataPrevious != null)
+			{
+				isSame = dataPrevious.SequenceEqual(transactionHistoryList,
+					new MbBankResponeHistoryTransactionDataEqualityComparer());
+			}
+			if (isSame) return;
+			//Have new recharge info
+			// Save new data into file
+			Util.WriteFile(directoryPathStoreData, transactionHistoryList);
+		}
+	}
+}
