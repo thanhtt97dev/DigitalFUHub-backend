83061079ae0e54f1e0c92bd16ff882e649b76352
hiuhihi78 <leduchieu2001x@gmail.com>
2023-12-06 15:17:53 +0700

    fix bugs

--- DigitalFUHubApi/Services/OpticalCharacterRecognitionService.cs
+++ DigitalFUHubApi/Services/OpticalCharacterRecognitionService.cs
@@ -1,4 +1,7 @@
 ﻿using Comons;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Extensions.Azure;
+using System;
 using System.Drawing;
 using System.Reflection.Metadata;
 using Tesseract;
@@ -14,45 +17,96 @@ namespace DigitalFUHubApi.Services
 #pragma warning disable CA1416 //bitmap just support for windown
 			byte[] bytes = System.Convert.FromBase64String(base64);
 
-			Bitmap image;
 			using (MemoryStream ms = new MemoryStream(bytes))
 			{
-				image = new Bitmap(ms);
-			}
-
-			if (image == null) return;
-
-			for (int y = 0; y < image.Height; y++)
-			{
-				for (int x = 0; x < image.Width; x++)
+				using (Bitmap image = new Bitmap(ms))
 				{
-					var pixel = image.GetPixel(x, y);
-					if (pixel.R < 112 && pixel.G < 112 && pixel.B < 112)
+					if (image == null) return;
+
+					for (int y = 0; y < image.Height; y++)
 					{
-						System.Drawing.Color black = System.Drawing.Color.FromArgb(255, 0, 0, 0);
-						image.SetPixel(x, y, black);
+						for (int x = 0; x < image.Width; x++)
+						{
+							var pixel = image.GetPixel(x, y);
+							if (pixel.R < 112 && pixel.G < 112 && pixel.B < 112)
+							{
+								System.Drawing.Color black = System.Drawing.Color.FromArgb(255, 0, 0, 0);
+								image.SetPixel(x, y, black);
+							}
+							else
+							{
+								System.Drawing.Color white = System.Drawing.Color.FromArgb(255, 255, 255, 255);
+								image.SetPixel(x, y, white);
+							}
+						}
 					}
-					else
-					{
-						System.Drawing.Color white = System.Drawing.Color.FromArgb(255, 255, 255, 255);
-						image.SetPixel(x, y, white);
-					}
+					string path = Constants.CAPTCHA_IMAGE_FILE_NAME;
+					image.Save(path);
 				}
 			}
-			string path = Constants.CAPTCHA_IMAGE_FILE_NAME;
-			image.Save(path);
 #pragma warning restore CA1416 // 
 		}
 
 		public string GetCaptchaInImage()
 		{
-			string path = Constants.CAPTCHA_IMAGE_FILE_NAME;
-			Pix pix = Pix.LoadFromFile(path);
+			using (var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default))
+			{
+				string path = Constants.CAPTCHA_IMAGE_FILE_NAME;
+				Pix pix = Pix.LoadFromFile(path);
 
-			var orc = new Tesseract.TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndLstm);
-			var page = orc.Process(pix);
+				var page = engine.Process(pix);
 
-			return page.GetText();
+				return page.GetText();
+			}
+		}
+
+		public byte[]? GetClarifyCaptchaImage(string base64)
+		{
+#pragma warning disable CA1416, SC8600 //bitmap just support for windown
+			byte[] bytes = System.Convert.FromBase64String(base64);
+
+			using (MemoryStream ms = new MemoryStream(bytes))
+			{
+				using (Bitmap image = new Bitmap(ms))
+				{
+					if (image == null) return null;
+
+					for (int y = 0; y < image.Height; y++)
+					{
+						for (int x = 0; x < image.Width; x++)
+						{
+							var pixel = image.GetPixel(x, y);
+							if (pixel.R < 112 && pixel.G < 112 && pixel.B < 112)
+							{
+								System.Drawing.Color black = System.Drawing.Color.FromArgb(255, 0, 0, 0);
+								image.SetPixel(x, y, black);
+							}
+							else
+							{
+								System.Drawing.Color white = System.Drawing.Color.FromArgb(255, 255, 255, 255);
+								image.SetPixel(x, y, white);
+							}
+						}
+					}
+
+					ImageConverter converter = new ImageConverter();
+					return (byte[]?)converter.ConvertTo(image, typeof(byte[]));
+				}
+			}
+#pragma warning restore CA1416, SC8600 // 
+		}
+
+
+		public string GetCaptchaInBase64Image(string base64)
+		{
+			using (var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default))
+			{
+				Pix pix = Pix.LoadFromMemory(GetClarifyCaptchaImage(base64));
+
+				var page = engine.Process(pix);
+
+				return page.GetText();
+			}
 		}
 	}
 }
