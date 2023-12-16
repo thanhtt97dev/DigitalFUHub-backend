2ce2ae69bbc19499103370f3162d33e1a7107f4c
hiuhihi78 <leduchieu2001x@gmail.com>
2023-12-16 11:16:10 +0700

    fix bugs

--- Comons/Constants.cs
+++ Comons/Constants.cs
@@ -112,8 +112,8 @@ namespace Comons
 
 		//Azure
 		public const string AZURE_ROOT_PATH = "https://fptu.blob.core.windows.net";
-		public const string AZURE_COMPUTER_VISION_ENDPOINT = "https://digitalfuhub-testing.cognitiveservices.azure.com";
-		public const string AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY = "7129b1df7ca944fd95027817f89bfc29";
+		public const string AZURE_COMPUTER_VISION_ENDPOINT = "https://digitalfuhub.cognitiveservices.azure.com";
+		public const string AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY = "60becab2ecde4f16917874641354099c";
 		public const string AZURE_EXTRACT_TEXT_FROM_IMAGE_API = $"{AZURE_COMPUTER_VISION_ENDPOINT}/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read&language=en";
 		public const string AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=fptu;AccountKey=RQLNtnNQb2tPDiqpZtac1dcMZMpI2AdsS63ZyAzMMar5FVfOsFUulBu27ecGae9LhK7O8unXV3z3+ASt64pCYA==;EndpointSuffix=core.windows.net";
 		public const string AZURE_STORAGE_CONTAINER_NAME = "test";
